ffade600d375d471d97e170463141a20081b0394
Eshwar-gowda <30590284+Eshwar1983@users.noreply.github.com>
2026-08-04 14:39:20 +0000

    New files

---
+++ main.py
@@ -0,0 +1,17 @@
+import os
+from fastapi import FastAPI
+from pymongo import MongoClient
+
+app = FastAPI()
+
+# Get URI from Render Environment Variables
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
+db = client["school_db"]
+collection = db["students"]
+
+
+@app.get("/data")
+def get_data():
+  documents = list(collection.find({}, {"_id": False}))
+  return {"data": documents}
