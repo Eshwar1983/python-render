70d783fd8536409e12f6f6ff69d51fe41624e12e
Eshwar-gowda <30590284+Eshwar1983@users.noreply.github.com>
2026-08-06 12:56:36 +0530

    revert

--- main.py
+++ main.py
@@ -1,26 +1,17 @@
 import os
-from flask import Flask  # or FastAPI, Streamlit, Dash, etc.
+from fastapi import FastAPI
 from pymongo import MongoClient
 
-app = Flask(__name__)
+app = FastAPI()
 
-# 1. Fetching user-defined environment variables from Render
-DATABASE_URL = os.environ.get("DATABASE_URL")
-client = MongoClient(DATABASE_URL)
+# Get URI from Render Environment Variables
+MONGO_URI = os.getenv("MONGO_URI")
+client = MongoClient(MONGO_URI)
 db = client["school_db"]
 collection = db["students"]
-API_SECRET = os.environ.get("API_KEY")
 
-# 2. Binding your host and port dynamically (Crucial for web hosting)
-# Render automatically injects a dynamic PORT variable at runtime.
-HOST = "0.0.0.0"
-PORT = int(os.environ.get("PORT", 10000))
 
-@app.get("/api")
+@app.get("/data")
 def get_data():
   documents = list(collection.find({}, {"_id": False}))
-  return {"api": documents}
-
-if __name__ == "__main__":
-    # Ensure your app binds to 0.0.0.0 and the assigned port
-    app.run(host=HOST, port=PORT)
+  return {"data": documents}
