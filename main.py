e8ba473928508c34e8ae57bcc01f0ef8e1e0b299
Eshwar-gowda <30590284+Eshwar1983@users.noreply.github.com>
2026-08-17 18:37:21 +0530

    Fix CORS middleware allow_origins syntax

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from pymongo import MongoClient
 app = FastAPI()
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=*,
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
